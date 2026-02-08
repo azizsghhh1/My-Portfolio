bec5118e105028e7e761f810cd7bf88e36d5bff5
paulo.henrique90 <91470722+doggbeats@users.noreply.github.com>
2026-02-08 08:52:24 +0800

    Atualizar o layout.tsx
    atualizando layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import { Orbitron } from 'next/font/google' // <-- nova fonte
 import AnimatedBackground from './components/AnimatedBackground'
+import ChatBot from "./components/ChatBot";
 
 const inter = Inter({ subsets: ['latin'] })  
 const orbitron = Orbitron({ subsets: ['latin'], weight: ['400','500','700','900'] }) // Orbitron
